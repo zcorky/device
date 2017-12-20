071927cf2e9cd920bca34f236b41ae339fe14e49
whatwewant <tobewhatwewant@gmail.com>
2017-12-20 11:28:05 +0800

    :sparkles: support sniff windows version

--- src/isLinux.ts
+++ src/isLinux.ts
@@ -4,3 +4,8 @@ export default function isLinux() {
   const ua: string = getUa();
   return /linux/i.test(ua);
 }
+
+// export function version() {
+//   const ua: string = getUa();
+//   return /linux/i.test(ua);
+// }
--- src/isWindows.ts
+++ src/isWindows.ts
@@ -4,3 +4,10 @@ export default function isWindows() {
   const ua: string = getUa();
   return /windows/i.test(ua);
 }
+
+export function version() {
+  const ua: string = getUa();
+  const _ = /windows\snt\s([\d\.]+)/i.exec(ua);
+
+  return _ ? ua[1] : null;
+}
